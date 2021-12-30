25feca07b0c0624c2851d21ae00dca2b12edd1a3
awesomeedl <82195917+awesomeedl@users.noreply.github.com>
2021-12-30 12:41:13 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const app = new PIXI.Application(800, 800, {backgroundColor:0x000000});
+
+document.body.appendChild(app.view);
+
+const loader = new PIXI.Loader();
+
+const orbContainer = new PIXI.Container();
+
+loader.add('orbDark', 'assets/Dark.png')
+    .add('orbLight', 'assets/Light.png')
+    .add('orbNature', 'assets/Nature.png')
+    .load(build);
+
+function build()
+{
+    const orbDark = app.loader.orbDark;
+    const orbLight = app.loader.orbLight;
+    const orbNature = app.loader.orbNature;
+
+    const orb1 = new PIXI.Sprite(orbDark);
+    const orb2 = new PIXI.Sprite(orbLight);
+    const orb3 = new PIXI.Sprite(orbNature);
+
+    container.addChild(orb1);
+    orb1.x = 0;
+    container.addChild(orb2);
+    orb2.x = 100;
+    container.addChild(orb3);
+    orb3.x = 200;
+}
+
+
+app.stage.addChild(orbContainer);
